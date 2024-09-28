930f877c8d02a81515dadd925a26f0d3192d9419
Rohit Pawar <rohitpawar5062@gmail.com>
2024-09-29 01:31:51 +0530

    7th change

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { isAuth } from '../middlewares/auth.js';
-import { newGroupChat } from '../controllers/chat.js';
+import { newGroupChat,getMyChats ,getMyGroups,addMembers,removeMembers,leaveGroup,sendAttachments} from '../controllers/chat.js';
+import {attachmentsMulter} from "../middlewares/multer.js"
 
 const app =express.Router()
 
@@ -10,6 +11,12 @@ const app =express.Router()
 app.use(isAuth)
 
 app.post("/new",newGroupChat)
+app.get("/my",getMyChats)
+app.get("/my/groups",getMyGroups)
+app.put("/addmembers",addMembers)
+app.put("/removemembers",removeMembers)
+app.delete("/leave/:id",leaveGroup)
+app.post("/message",attachmentsMulter,sendAttachments)
 
 
 export default app;
